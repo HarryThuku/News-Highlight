4b9218d9802b23832a03280c5cdd53d18e7bfb38
HarryThuku <harsygeorgee@gmail.com>
2019-04-27 18:33:34 +0300

    adds data source url for api calls.

--- config.py
+++ config.py
@@ -3,6 +3,12 @@ import os
 class Config:
     '''
     '''
+    SOURCES_URL = 'https://newsapi.org/v2/sources?language=en&apiKey={}'
+    NEWS_BY_SOURCES_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
+    TOP_HEADLINES_URL = 'https://newsapi.org/v2/top-headlines?country=us&category={}&apiKey={}' # it displays news items filtered by category. should be used in the home-page.
+    SEARCH_URL = 'https://newsapi.org/v2/everything?q={}apiKey={}'
+    API_KEY = os.environ.get('API_KEY')
+
 
 class DevConfig(Config):
     '''
@@ -14,3 +20,9 @@ class ProdConfig(Config):
     '''
     '''
     pass
+
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig
+}
